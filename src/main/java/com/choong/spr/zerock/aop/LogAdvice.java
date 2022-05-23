8c6a08e89a0bcfc054835ff79f028ed7a9a20eca
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-05-23 23:55:14 +0900

    AOP조금 공부.ㅠ

--- src/main/java/com/choong/spr/zerock/aop/LogAdvice.java
+++ src/main/java/com/choong/spr/zerock/aop/LogAdvice.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.log4j.Log4j;
 
-@Aspect
-@Log4j
-@Component
+@Aspect // 해당 클래스가  Aspect를 구현함을 나타냄
+@Log4j 
+@Component // 스프링에서 빈으로 인식하기위해 사용
 public class LogAdvice {
-	@Before( "execution(* org.zerock.service.SampleService*.*(..))")
-	public void logBefore() {
+	@Before( "execution(* org.zerock.service.SampleService*.*(..))") // Point Cut으로 ( )안은 AspectJ의 표현식임. excution은 접근제한자와 특수정 클래스의 메서드를 지정할 수 있
+	public void logBefore() {                                       // 맨앞의 *은 접근제한자를 의미, 맨두의 *은 클래스의 이름과 메서드의 이름을 의미함.
 		log.info("======================");
 	}
 }
