04f613bf2bd0086706eda80346eecd99d1dc4351
hyunsu <violin0117@naver.com>
2022-06-02 15:16:38 +0900

    aws s3에 업로드(저장)

--- src/main/java/com/choong/spr/service/BoardService.java
+++ src/main/java/com/choong/spr/service/BoardService.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +16,12 @@ import org.springframework.web.multipart.MultipartFile;
 import com.choong.spr.domain.BoardDto;
 import com.choong.spr.mapper.BoardMapper;
 import com.choong.spr.mapper.ReplyMapper;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 @Service
 public class BoardService {
 
@@ -22,10 +31,24 @@ public class BoardService {
 	@Autowired
 	private ReplyMapper replyMapper;
 	
+	@Value("${aws.s3.bucketName}")
+	private String bucketName;
+	
+	private S3Client s3; 
+	
+	@PostConstruct
+	public void init() {
+		Region region = Region.AP_NORTHEAST_2;
+		this.s3 = S3Client.builder()
+						.region(region)
+						.build();
+	}
+	
 	public List<BoardDto> listBoard(String type, String keyword) {
 		// TODO Auto-generated method stub
 		return mapper.selectBoardAll(type, "%"+keyword+"%");
 	}
+	
 	
 	@Transactional
 	public boolean insertBoard(BoardDto board, MultipartFile file) {
@@ -37,12 +60,39 @@ public class BoardService {
 		// 파일 등록
 		if(file.getSize() > 0) {
 			mapper.insertFile(board.getId(), file.getOriginalFilename());
-			saveFile(board.getId(), file); // 현재는 desk탑에 저장하지만 aws에 저장하도록 해야 함.
+			// saveFile(board.getId(), file); // 현재는 desk탑에 저장하지만 aws에 저장하도록 해야 함.
+			saveFileAwsS3(board.getId(), file); // aws s3에 업로드(저장)
 		}
 		
 		return cnt == 1;
 	}
-
+	
+	
+	// aws s3에 업로드(저장)
+	private void saveFileAwsS3(int id, MultipartFile file) {
+		// board/37/12344.png
+		String key = "board/" + id + "/" + file.getOriginalFilename();
+		
+		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+				.acl(ObjectCannedACL.PUBLIC_READ) 		 // acl : 권한 설정
+				.bucket(bucketName) 					// bucket 위치 설정
+				.key(key)								// 키
+				.build(); 								 // 이를 통해 PutObjectRequest객체 만듬
+		
+		RequestBody requestBody;
+		try {
+			requestBody = RequestBody.fromInputStream(file.getInputStream(), file.getSize());
+			s3.putObject(putObjectRequest, requestBody);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e); // 트랜잭션 때문에 모두 실패????????????
+		}
+		
+	}
+	
+	
+	
+	// desk탑에 업로드(저장)
 	private void saveFile(int id, MultipartFile file) {
 		String pathStr = "C:/imgtmp/board/" + id + "/"; // 새로운 폴더 만드는 코드
 		File path = new File(pathStr);
